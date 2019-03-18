50baad13ba6b861a658f45c4715a01abb851d6fd
Caius Durling <dev@caius.name>
2019-03-18 23:34:42 +0000

    Look up image by uuid

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,15 @@
 data "smartos_image" "base_64_2013Q2" {
-  name    = "base64"
-  version = "13.2.0"
+  uuid = "0084dad6-05c1-11e3-9476-8f8320925eea"
 }
+
+/*data "smartos_zone" "test1" {
+  uuid = "3fced63c-ce05-404f-f635-ecff8144ce55"
+}*/
+
+
+/*resource "smartos_zone" "test1" {
+  name       = "test1"
+  image_uuid = "${data.smartos_image.base_64_2013Q2.id}"
+  type       = "os"
+}*/
+
